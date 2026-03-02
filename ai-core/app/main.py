bd000d43551e4e1a062ba0a80a9435665101e8dd
Dev <Dev.Sterling@outlook.com>
2026-03-02 20:16:20 +0530

    Bug fixes

--- ai-core/app/main.py
+++ ai-core/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, HTTPException
 import httpx
-import feedparser
+#import feedparser
 
 app = FastAPI()
 
-JAVA_RSS_URL = "http://127.0.0.1:8080/rss-test"
+JAVA_RSS_URL = "http://localhost:9090/news"
+
+@app.get('/')
+async def root():
+    return {"Message": "Hello"}
 
 @app.get("/archnews")
 async def archnews():
@@ -15,20 +19,4 @@ async def archnews():
         except httpx.HTTPError as e:
             raise HTTPException(status_code=502, detail=str(e))
 
-    feed = feedparser.parse(r.text)
-
-    return {
-        "title": feed.feed.get("title"),
-        "link": feed.feed.get("link"),
-        "description": feed.feed.get("description"),
-        "last_updated": feed.feed.get("updated"),
-        "items": [
-            {
-                "title": entry.get("title"),
-                "link": entry.get("link"),
-                "published": entry.get("published"),
-                "summary": entry.get("summary"),
-            }
-            for entry in feed.entries
-        ],
-    }
+    return r.json()
